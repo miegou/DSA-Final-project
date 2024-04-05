3a2f897bf0ba675f91bc91e32e5d224b8aabd1f5
miegou <112467763+miegou@users.noreply.github.com>
2024-04-05 13:40:02 +0300

    commitcommit

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 #include "csv_reader.h"
 
 #define MAX_SARAKE 30
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Käyttö: %s <tiedostonimi.csv>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -21,12 +23,14 @@ int main(int argc, char *argv[]) {
     int valinta;
     printf("Montako riviä haluat lukea tiedostosta? (enintään %d)\n", rivien_lkm);
     scanf("%d", &valinta);
-    if (valinta > rivien_lkm || valinta < 1) {
+    if (valinta > rivien_lkm || valinta < 1)
+    {
         fprintf(stderr, "Virheellinen syöte.\n");
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < valinta; i++) {
+    for (int i = 0; i < valinta; i++)
+    {
         root = insertNode(root, rivit[i]->arvot[0]);
     }
 
@@ -35,17 +39,18 @@ int main(int argc, char *argv[]) {
     printf("\n");
 
     // Vapautetaan varatut muistialueet
-    for (int i = 0; i < valinta; i++) {
-        for (int j = 0; j < MAX_SARAKE; j++) { // Korjattu MAX_SARAKE
+    for (int i = 0; i < valinta; i++)
+    {
+        for (int j = 0; j < MAX_SARAKE; j++)
+        { // Korjattu MAX_SARAKE
             free(rivit[i]->arvot[j]);
         }
         free(rivit[i]);
     }
     free(rivit);
-    
+
     freeAVL(root);
     free(sarakkeet);
 
     return 0;
 }
-
